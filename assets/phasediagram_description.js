18d70390fdaea46a2c160457a6aca3da64fad348
gabgoh <gabgohjh@gmail.com>
2017-03-04 02:09:39 -0800

    More updates to "critical damping"

--- assets/phasediagram_description.js
+++ assets/phasediagram_description.js
@@ -1,17 +1,36 @@
-function phaseDiagram(divin) {
+function phaseDiagram_dec(divin) {
 
   var totalIters = 150
   var default_underdamp = 0.97
   var default_overdamp = 0.05
 
+  function myRunMomentum(f, w0, alpha, beta, totalIters) {
+    var W = []; var Z = []; var z = zeros(w0.length); var w = w0
+    z[1] = 1
+    var gx = f(w0);
+    W.push(w0); 
+    Z.push(z);
+    for (var i = 0; i < totalIters; i++) {
+      var z = numeric.add(numeric.mul(beta, z), gx)
+      var w = numeric.add(w, numeric.mul(-alpha, z))
+      gx = f(w); 
+      if (w.every(isFinite)) {
+        W.push(w)
+        Z.push(z)
+      } else{ console.log("breaking"); break; }
+    }
+    return [Z,W]
+  }
+
   function getTrace(alpha, beta, lambda) {
     var m = []
-    var iter = geniterMomentum([[1, 0],[0, 1]], lambda, [1,1*100], alpha, beta)
+    var iter = myRunMomentum(function(x) { return [x[0],lambda*x[1]] }, [2,0.92], alpha, beta, totalIters) 
     // run for 500 iterations
+    console.log(iter)
     for (var i = 0; i <= totalIters; i++) { 
-      var x = numeric.add(iter(i)[1],-1*1)
-      var y = numeric.mul(iter(i)[0],(1/200))
-      m.push([y[1], x[1]])
+      var x = -1*iter[1][i][1]
+      var y = (1.4)*iter[0][i][1]
+      m.push([y, x])
     }
     mG = m
     return m
@@ -34,20 +53,20 @@ function phaseDiagram(divin) {
 
   var overlay = divin.append("svg")
        .style("position", "absolute")
-       .attr("width", 648)
+       .attr("width", 348)
        .attr("height", 520)
        .style("z-index", 10)
        .style("pointer-events", "none")
 
   // Draw the three phases
-  for (var i = 0; i < 3; i ++ ) {
+  for (var i = 0; i < 4; i ++ ) {
 
     var div = divin.append("div")
       .style("position","absolute")
       .style("width",w + "px")
       .style("height",h + "px")
-      .style("left", [15, 235, 455][i] + "px")
-      .style("top", [110, 110, 110][i] + "px")
+      .style("left", [15, 265, 15, 265][i] + "px")
+      .style("top", [10, 10, 360, 360][i] + "px")
 
     var svg = div.append("svg")
                 .style("position", 'absolute')
@@ -78,7 +97,7 @@ function phaseDiagram(divin) {
                   .circleRadius(1.5) 
                   .stroke(colorbrewer.OrRd[3][0])(svg)
 
-    update(getTrace(al, [0.01, optbeta + 0.0001 , default_underdamp][i], [0,10]))
+    update(getTrace(0.02, [1,0.97,1,0.97][i], [0,0,0.25,0.25][i]))
 
   }
 
